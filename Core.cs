d8baa0b131bc8106791a74d2f82b7ee4ed2c6a83
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-22 15:21:09 -0600

    Boilerplate

---
+++ Core.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(JPInstaller.Core), "JPInstaller", "1.0.0", "taldo", null)]
+[assembly: MelonGame("Videocult", "Airframe")]
+
+namespace JPInstaller
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
